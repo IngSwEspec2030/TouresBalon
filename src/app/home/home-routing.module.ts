06646edf6cfb05868a1777a19eb846434481fc41
Juan Pablo Reyes Cardenas <juanpabloreyescardenas@gmail.com>
2020-09-03 12:15:51 -0500

    fix

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+    children: [
+      {
+        path: 'plan',
+        loadChildren: () => import('../plans/plans.module').then( m => m.PlansPageModule)
+      },
+      {
+        path: 'event',
+        loadChildren: () => import('../events/events.module').then( m => m.EventsPageModule)
+      },
+      {
+        path: 'transport',
+        loadChildren: '../plans/plans.module#PlansPageModule'
+      },
+      {
+        path: 'hotel',
+        loadChildren: '../plans/plans.module#PlansPageModule'
+      },
+      {
+        path: 'cart',
+        loadChildren: '../plans/plans.module#PlansPageModule'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'plan',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomePageRoutingModule {}
